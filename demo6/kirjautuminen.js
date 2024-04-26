c879ae90c7cb84b9bc3cbfd654f761d360aaedf6
jennirupponenkeuda <jenni.rupponen@edu.keuda.fi>
2024-04-26 17:56:46 +0300

    Korjattu koodia vastaamaan tehtävänantoa

---
+++ demo6/kirjautuminen.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut() {
+   
+    let kirjautunut = 'kylla'; 
+    if (kirjautunut === 'kylla') {
+        document.getElementById('tervetulo_teksti').textContent += ' Jenni! ';
+    }
+}
