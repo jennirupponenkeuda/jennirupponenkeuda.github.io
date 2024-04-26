c580f5c59a8daad5c049c893ec899f08c6253120
jennirupponenkeuda <jenni.rupponen@edu.keuda.fi>
2024-04-26 18:27:30 +0300

    Muutoksia nimiasetuksiin ja välilyönti

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -4,7 +4,7 @@ function onkoKirjautunut() {
    
     let kirjautunut = 'kylla'; 
     if (kirjautunut === 'kylla') {
-        document.getElementById('tervetulo_teksti').textContent += ' Jenni! ';
+        document.getElementById('tervetulo_teksti').textContent += " " + localStorage.getItem("nimi");
     }
 }
 
