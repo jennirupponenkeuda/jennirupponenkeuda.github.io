f75fd6db5521f5b48eebb949be7e7fb2d2fe8548
jennirupponenkeuda <jenni.rupponen@edu.keuda.fi>
2024-04-29 08:23:00 +0300

    viimeistelty kirjaudu ulos -napin näkyvyyttä

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+document.addEventListener("DOMContentLoaded", onkoKirjautunut)
 
 function onkoKirjautunut() {  
     if (localStorage.getItem("kirjautunut") === 'kylla') {
         document.getElementById('tervetulo_teksti').textContent += " " + localStorage.getItem("nimi");
         document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("kirjaudu_ulos_nappi").style.display = "block";
 
     }
 }
@@ -13,6 +14,7 @@ function kirjaudu(){
     localStorage.setItem("kirjautunut", "kylla");
     document.getElementById("kirjautumis_lomake").style.display = "none";
     document.getElementById("kirjaudu_ulos_nappi").style.display = "block";
+    
 }
 
 function kirjauduUlos() {
